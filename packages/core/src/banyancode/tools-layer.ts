02116318b85f88ec2844f279f79b7ac501ff4f04
Ekagra <ekagra2006@gmail.com>
2026-06-13 23:29:12 +0530

    feat(banyancode): wire banyan tools into single location layer
    Compose all BanyanCode tools and services into a single BanyanTools.locationLayer that consumers can opt into.

---
+++ packages/core/src/banyancode/tools-layer.ts
@@ -0,0 +1,35 @@
+export * as BanyanTools from "./tools-layer"
+
+import { Layer } from "effect"
+import { CodegraphTools } from "../tool/codegraph"
+import { CodeEmbedTools } from "../tool/code-embed"
+import { MemoryTools } from "../tool/memory"
+import { SharedMemoryTool } from "../tool/shared-memory"
+import { SubagentMessageTool } from "../tool/subagent-message"
+import { SystemStatusTool } from "../tool/system-status"
+import { defaultLayer as memoryRepoLayer } from "./memory-repo"
+import { defaultLayer as subagentBusLayer } from "./subagent-bus"
+import { defaultLayer as embeddingProviderLayer } from "./embedding-provider"
+import { defaultLayer as codegraphRepoLayer } from "./codegraph-repo"
+import { defaultLayer as codegraphIndexerLayer } from "./codegraph-indexer"
+import { defaultLayer as codegraphAnalyzerLayer } from "./codegraph-analyzer"
+import { defaultLayer as codegraphEmbedderLayer } from "./codegraph-embedder"
+import { defaultLayer as systemMonitorLayer } from "./system-monitor"
+
+export const locationLayer = Layer.mergeAll(
+  SharedMemoryTool.layer,
+  SubagentMessageTool.layer,
+  MemoryTools.locationLayer,
+  CodegraphTools.locationLayer,
+  CodeEmbedTools.locationLayer,
+  SystemStatusTool.layer,
+).pipe(
+  Layer.provide(subagentBusLayer),
+  Layer.provide(memoryRepoLayer),
+  Layer.provide(embeddingProviderLayer),
+  Layer.provide(codegraphRepoLayer),
+  Layer.provide(codegraphIndexerLayer),
+  Layer.provide(codegraphAnalyzerLayer),
+  Layer.provide(codegraphEmbedderLayer),
+  Layer.provide(systemMonitorLayer),
+)
